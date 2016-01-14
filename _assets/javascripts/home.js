c66b8c0b951ff73e75508aaeb22d677d376dffbe
Chris Wallwork <chris@unrefgugees.org>
2016-01-14 11:01:03 -0500

    moved to prod creds, added cloudfront distro to s3_website, uglified css and js

--- _assets/javascripts/home.js
+++ _assets/javascripts/home.js
@@ -1,7 +1,7 @@
 'user-strict'
 var gw = new Groundwork({
   'api_url': 'https://api.thegroundwork.com',
-  'oauth_client_id': 'pub-un-test.climate-change-test-int-7r9Hc3E7DKCjMgGwTXlHGSN_CXRfF3q4qhAYjYSS7I6jiCCYyxN3VfCS.GkOVmBTM27MGKV8PhzNdhTNxR3qSA'
+  'oauth_client_id': 'pub-un.climate-change-int-FKQNCktMwjde7J6UUnKf8eEZL7FCNIaS5Jem7Mj79dmNGsTOp1vCfI80IyjrFtcPMQ_jr.9QDxI07qYwunPFIA'
 });
 
 function sendData(data){
